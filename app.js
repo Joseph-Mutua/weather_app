aaf15b705612571cf850df48e4b0b7f99cb8e295
Joseph Mutua <mutuaj793@gmail.com>
2020-07-17 20:45:28 +0000

    Modify existing code

--- app.js
+++ app.js
@@ -11,3 +11,49 @@ setTimeout(() => {
 }, 0);
 
 log("Stopping!");
+
+
+
+const fs = require('fs')
+const chalk = require('chalk')
+const log = console.log;
+
+
+const addNote = (title, body) => {
+    const notes = loadNotes();
+    const duplicateNote = notes.find((note) => note.title === title);
+
+    if (!duplicateNote) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        log(chalk.green.inverse('New note added!'))
+    }
+    else {
+        log(chalk.red.inverse('Note title taken!'))
+    }
+}
+
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter((note) => note.title !== title);
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.green.inverse('Note removed!'))
+        saveNotes(notesToKeep)
+    }
+    else {
+        log(chalk.red.inverse('No note found!'))
+    }
+};
+
+const listNotes = () => {
+    const notes = loadNotes();
+    log(chalk.inverse("YOUR NOTES"));
+    notes.forEach((note) => {
+        log(chalk.bold.yellow.underline(note.title));
+        log(chalk.blue(note.body));
+    });
+}
