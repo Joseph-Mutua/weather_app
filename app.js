24d45a4bf3a338e345eb7d5f058415197e334a70
Joseph Mutua <mutuaj793@gmail.com>
2020-07-18 15:37:32 +0000

    Use metric units

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const log = console.log;
-const url = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&appid=d18f6e53f610b112cec2414b0a7f9b39";
+const url = "https://api.openweathermap.org/data/2.5/onecall?&units=metric&lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&appid=d18f6e53f610b112cec2414b0a7f9b39";
+
 
 request({ url: url, json: true }, (error, response) => {
     const currWeather = response.body.current;
