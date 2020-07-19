83c5de732ff0995a74cc4cb98c3fd4e55e9cf2c1
Joseph Mutua <mutuaj793@gmail.com>
2020-07-19 19:28:01 +0000

    Customizing HTTP requests

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const request = require('request');
 const log = console.log;
-const url = "https://api.openweathermap.org/data/2.5/onecall?&units=metric&lat=33.441792&lon=-94.037689&%20exclude=hourly,daily&appid=d18f6e53f610b112cec2414b0a7f9b39";
+const url = "https://api.openweathermap.org/data/2.5/onecall?&units=metric&lat=33.441792&lon=-94.037689&%20&exclude=hourly,daily&appid=d18f6e53f610b112cec2414b0a7f9b39";
 
 
 request({ url: url, json: true }, (error, response) => {
     const currWeather = response.body.current;
+    log(response.body.minutely);
     log(`It is currently ${currWeather.temp} out. The humidity is about ${currWeather.humidity}`);
 });
