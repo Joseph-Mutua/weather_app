1896afa436e98e204de660b6880d1a83b4048890
Joseph Mutua <mutuaj793@gmail.com>
2020-07-20 11:59:48 +0000

    Add geocode function for fetching location data

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 const log = console.log;
 const url = "https://api.openweathermap.org/data/2.5/onecall?&units=metric&lat=33.441792&lon=-94.037689&%20&exclude=hourly,daily&appid=d18f6e53f610b112cec2414b0a7f9b39";
-const mapBoxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicWZ0b2dwZGNuc3giLCJhIjoiY2tjdGhqcjd1MHNtdjJ5cDR6eHl2cTVmZiJ9.5Gz-8waHwoI80WFZqzr0mQ";
+const geocode = require('./utils/geocode');
 
 request({ url: url, json: true }, (error, response) => {
     if (error) {
@@ -18,16 +18,7 @@ request({ url: url, json: true }, (error, response) => {
 });
 
 
-request({ url: mapBoxUrl, json: true }, (error, response) => {
-    if (error) {
-        log("Unable to Connect to Location Services!");
-    }
-    else if (!response.body.features) {
-        log("Please Specify the Location");
-    }
-    else {
-        const locationLong = response.body.features[0].geometry.coordinates[0];
-        const locationLat = response.body.features[0].geometry.coordinates[1];
-        log(`The longitude is ${locationLong} while the latitude is ${locationLat}`);
-    }
-});
+geocode('Kitui', (error, data) => {
+    log('Error', error);
+    log('Data', data);
+})
